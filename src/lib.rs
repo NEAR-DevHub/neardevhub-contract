06ce89a2841f3d8acef67b210a4b914de903a121
Maksym Zavershynskyi <max@near.org>
2022-11-29 14:59:33 -0800

    Seemingly complete business logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,50 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::{LookupMap, Vector};
+use near_sdk::collections::Vector;
 use near_sdk::serde::{Deserialize, Serialize};
-use near_sdk::{
-    env, near_bindgen, AccountId, Balance, BorshStorageKey, PanicOnDefault, Promise,
-    PromiseOrValue, Timestamp,
-};
+use near_sdk::{env, near_bindgen, AccountId, Balance, BorshStorageKey, PanicOnDefault, Timestamp};
 
 near_sdk::setup_alloc!();
 
+type IdeaId = u64;
 type AttestationId = u64;
-type AttesterId = AccountId;
 type SubmissionId = u64;
+type SponsorshipId = u64;
 
 #[derive(BorshSerialize, BorshStorageKey)]
 enum StorageKey {
     Ideas,
     Submissions,
-    IdeaToSubmissions,
     Attestations,
     Sponsorships,
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+enum SponsorshipToken {
+    Native,
+    NEP141 { address: AccountId },
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct Sponsorship {
+    sponsorship_id: SponsorshipId,
+    idea_id: IdeaId,
+    sponsor: AccountId,
+    timestamp: Timestamp,
+    description: String,
+    sponsorship_token: SponsorshipToken,
+    amount: Balance,
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct SponsorshipInput {
+    idea_id: IdeaId,
+    sponsor: AccountId,
+    description: String,
+    sponsorship_token: SponsorshipToken,
+    amount: Balance,
 }
 
 #[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
@@ -44,6 +70,8 @@ pub struct Idea {
     description: String,
     submitter_id: AccountId,
     timestamp: Timestamp,
+    submissions: Vec<SubmissionId>,
+    sponsorships: Vec<SponsorshipId>,
 }
 
 #[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize)]
@@ -80,6 +108,7 @@ pub struct Submission {
     #[serde(with = "u64_dec_format")]
     timestamp: Timestamp,
     attestations: Vec<AttestationId>,
+    sponsorships: Vec<SponsorshipId>,
 }
 
 #[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize)]
@@ -92,21 +121,23 @@ pub struct SubmissionInput {
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Contract {
+    pub treasury: AccountId,
     pub ideas: Vector<Idea>,
     pub submissions: Vector<Submission>,
-    pub idea_to_submissions: LookupMap<u64, Vec<SubmissionId>>,
     pub attestations: Vector<Attestation>,
+    pub sponsorships: Vector<Sponsorship>,
 }
 
 #[near_bindgen]
 impl Contract {
     #[init]
-    pub fn new() -> Self {
+    pub fn new(alternative_treasury: Option<AccountId>) -> Self {
         Self {
+            treasury: alternative_treasury.unwrap_or(env::current_account_id()),
             ideas: Vector::new(StorageKey::Ideas),
             submissions: Vector::new(StorageKey::Submissions),
-            idea_to_submissions: LookupMap::new(StorageKey::IdeaToSubmissions),
             attestations: Vector::new(StorageKey::Attestations),
+            sponsorships: Vector::new(StorageKey::Sponsorships),
         }
     }
 
@@ -116,18 +147,20 @@ impl Contract {
             description,
             submitter_id: env::predecessor_account_id(),
             timestamp: env::block_timestamp(),
+            submissions: vec![],
+            sponsorships: vec![],
         })
     }
 
-    pub fn get_idea(&self, idea_id: u64) -> IdeaOutput {
+    pub fn get_idea(&self, idea_id: IdeaId) -> IdeaOutput {
         IdeaOutput::from(idea_id, self.ideas.get(idea_id).unwrap())
     }
 
-    pub fn get_num_ideas(&self) -> u64 {
+    pub fn get_num_ideas(&self) -> IdeaId {
         self.ideas.len()
     }
 
-    pub fn get_ideas(&self, from_index: Option<u64>, limit: Option<u64>) -> Vec<IdeaOutput> {
+    pub fn get_ideas(&self, from_index: Option<IdeaId>, limit: Option<IdeaId>) -> Vec<IdeaOutput> {
         let from_index = from_index.unwrap_or(0);
         let limit = limit.unwrap_or(self.ideas.len());
         (from_index..std::cmp::min(from_index + limit, self.ideas.len()))
@@ -137,9 +170,9 @@ impl Contract {
 
     pub fn add_submission(&mut self, submission_inp: SubmissionInput) {
         let idea_id = submission_inp.idea_id;
-        let mut submission_ids = self.idea_to_submissions.get(&idea_id).unwrap_or_default();
-        submission_ids.push(self.submissions.len());
-        self.idea_to_submissions.insert(&idea_id, &submission_ids);
+        let mut idea = self.ideas.get(idea_id).expect("Submission id not found");
+        idea.submissions.push(self.submissions.len());
+        self.ideas.replace(idea_id, &idea);
 
         self.submissions.push(&Submission {
             idea_id: submission_inp.idea_id,
@@ -147,18 +180,20 @@ impl Contract {
             description: submission_inp.description,
             timestamp: env::block_timestamp(),
             attestations: vec![],
+            sponsorships: vec![],
         });
     }
 
-    pub fn get_submissions(&self, idea_id: u64) -> Vec<Submission> {
-        let ids = self.idea_to_submissions.get(&idea_id).unwrap_or_default();
-        ids.into_iter().map(|id| self.submissions.get(id).unwrap()).collect()
+    pub fn get_submissions(&self, idea_id: IdeaId) -> Vec<Submission> {
+        let idea = self.ideas.get(idea_id).expect("Idea id not found");
+        idea.submissions.iter().map(|id| self.submissions.get(*id).unwrap()).collect()
     }
 
     pub fn add_attestation(&mut self, attestation_inp: AttestationInput) {
+        let new_attestation_id = self.attestations.len();
+
         let mut submission =
             self.submissions.get(attestation_inp.submission_id).expect("Submission id not found");
-        let new_attestation_id = self.attestations.len();
         submission.attestations.push(new_attestation_id);
         self.submissions.replace(attestation_inp.submission_id, &submission);
 
@@ -174,6 +209,29 @@ impl Contract {
     pub fn get_attestations(&self, submission_id: SubmissionId) -> Vec<Attestation> {
         let submission = self.submissions.get(submission_id).expect("Submission id not found");
         submission.attestations.iter().map(|id| self.attestations.get(*id).unwrap()).collect()
+    }
+
+    pub fn add_sponsorship(&mut self, sponsorship_inp: SponsorshipInput) {
+        let new_sponsorship_id = self.sponsorships.len();
+
+        let mut idea = self.ideas.get(sponsorship_inp.idea_id).expect("Idea id not found");
+        idea.sponsorships.push(new_sponsorship_id);
+        self.ideas.replace(sponsorship_inp.idea_id, &idea);
+
+        self.sponsorships.push(&Sponsorship {
+            sponsorship_id: new_sponsorship_id,
+            idea_id: sponsorship_inp.idea_id,
+            sponsor: sponsorship_inp.sponsor,
+            timestamp: env::block_timestamp(),
+            description: sponsorship_inp.description,
+            sponsorship_token: sponsorship_inp.sponsorship_token,
+            amount: sponsorship_inp.amount,
+        });
+    }
+
+    pub fn get_sponsorships(&self, idea_id: IdeaId) -> Vec<Sponsorship> {
+        let idea = self.ideas.get(idea_id).expect("Idea id not found");
+        idea.sponsorships.iter().map(|id| self.sponsorships.get(*id).unwrap()).collect()
     }
 }
 
