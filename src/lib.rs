63a3e1da557dddfd8617442f67e119ac26d94d40
Maksym Zavershynskyi <max@near.org>
2022-12-08 17:28:21 -0800

    Fix comments backward compatibility

--- src/lib.rs
+++ src/lib.rs
@@ -289,6 +289,23 @@ pub struct Contract {
 
 #[near_bindgen]
 impl Contract {
+    /// This code was called only once to upgrade the contract to contain comments.
+    pub fn initiate_comments() {
+        assert_eq!(
+            env::current_account_id(),
+            env::predecessor_account_id(),
+            "Can only be called by the account itself"
+        );
+        let v: Vector<Comment> = Vector::new(StorageKey::Comments);
+        let data = v.try_to_vec().expect("Cannot serialize the contract state.");
+        env::storage_write(
+            &StorageKey::Comments.try_to_vec().expect("Cannot serialize comments key"),
+            &data,
+        );
+
+        env::state_write(&Self::new());
+    }
+
     #[init]
     pub fn new() -> Self {
         Self {
