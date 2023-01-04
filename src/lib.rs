1e0506a724096e672fd85aa105c0904904891d2e
Maksym Zavershynskyi <max@near.org>
2023-01-04 15:14:46 -0800

    Index labels and add post editing

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ pub mod str_serializers;
 
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::{LookupMap, UnorderedMap, Vector};
-use near_sdk::{env, near_bindgen, PanicOnDefault};
+use near_sdk::{env, near_bindgen, AccountId, PanicOnDefault};
 use post::*;
 use std::collections::HashSet;
 
@@ -79,11 +79,19 @@ impl Contract {
         self.posts.replace(post_id, &post.into());
     }
 
-    pub fn add_post(&mut self, parent_id: Option<PostId>, body: PostBody, labels: HashSet<Label>) {
+    pub fn add_post(&mut self, parent_id: Option<PostId>, body: PostBody, labels: HashSet<String>) {
         let parent_id = parent_id.unwrap_or(ROOT_POST_ID);
         let id = self.posts.len();
         let author_id = env::predecessor_account_id();
         let editor_id = author_id.clone();
+
+        for label in &labels {
+            let mut other_posts = self.label_to_posts.get(label).unwrap_or_default();
+            other_posts.insert(id);
+            self.label_to_posts.insert(label, &other_posts);
+        }
+        let labels = labels.into_iter().map(|name| Label { name }).collect();
+
         let post = Post {
             id,
             author_id,
@@ -97,5 +105,64 @@ impl Contract {
         let mut siblings = self.post_to_children.get(&parent_id).expect("Parent id not found");
         siblings.push(id);
         self.post_to_children.insert(&parent_id, &siblings);
+    }
+
+    pub fn get_posts_by_label(&self, label: String) -> Vec<PostId> {
+        let mut res: Vec<_> =
+            self.label_to_posts.get(&label).unwrap_or_default().into_iter().collect();
+        res.sort();
+        res
+    }
+
+    pub fn get_all_labels(&self) -> Vec<String> {
+        let mut res: Vec<_> = self.label_to_posts.keys().collect();
+        res.sort();
+        res
+    }
+
+    pub fn is_allowed_to_edit(&self, post_id: PostId, editor: Option<AccountId>) -> bool {
+        let post: Post = self.posts.get(post_id).expect("Post id not found").into();
+        let editor = editor.unwrap_or(env::predecessor_account_id());
+        // TODO: Allow moderators to edit posts.
+        editor == env::current_account_id() || editor == post.author_id
+    }
+
+    pub fn edit_post(&mut self, id: PostId, body: PostBody, labels: HashSet<String>) {
+        assert!(
+            self.is_allowed_to_edit(id, Option::None),
+            "The account is not allowed to edit this post"
+        );
+        let editor_id = env::predecessor_account_id();
+        let mut post: Post = self.posts.get(id).expect("Post id not found").into();
+
+        let old_snapshot = post.snapshot.clone();
+        let old_labels_set = old_snapshot.labels.clone();
+        let new_labels: HashSet<_> = labels.into_iter().map(|name| Label { name }).collect();
+        let new_snapshot = PostSnapshot {
+            editor_id,
+            timestamp: env::block_timestamp(),
+            labels: new_labels.clone(),
+            body,
+        };
+        post.snapshot = new_snapshot;
+        post.snapshot_history.push(old_snapshot);
+        self.posts.push(&post.into());
+
+        // Update labels index.
+
+        let new_labels_set = new_labels;
+        let labels_to_remove = &old_labels_set - &new_labels_set;
+        let labels_to_add = &new_labels_set - &old_labels_set;
+        for label_to_remove in labels_to_remove {
+            let mut posts = self.label_to_posts.get(&label_to_remove.name).unwrap();
+            posts.remove(&id);
+            self.label_to_posts.insert(&label_to_remove.name, &posts);
+        }
+
+        for label_to_add in labels_to_add {
+            let mut posts = self.label_to_posts.get(&label_to_add.name).unwrap();
+            posts.insert(id);
+            self.label_to_posts.insert(&label_to_add.name, &posts);
+        }
     }
 }
