6220348323b769c6dd0f7f14b05b9a34f7f50ab3
Maksym Zavershynskyi <max@near.org>
2023-02-07 17:45:38 -0800

    Fix build script

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 set -e
 
 RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
-cp target/wasm32-unknown-unknown/release/near_ideas.wasm ./res/
+cp target/wasm32-unknown-unknown/release/devgovgigs.wasm ./res/
 
