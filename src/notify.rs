eccf5a1256bdebb938dae032cd77ef441c977193
Peter Salomonsen <petersalomonsen@runbox.com>
2023-06-11 19:08:29 +0000

    unit tests for mention

--- src/notify.rs
+++ src/notify.rs
@@ -91,3 +91,62 @@ fn notify(post_id: PostId, post_author: AccountId, action: &str) -> Promise {
         env::prepaid_gas() / 4,
     )
 }
+
+#[cfg(all(test, not(target_arch = "wasm32")))]
+mod tests {
+    use std::convert::TryInto;
+
+    use super::notify_mentions;
+
+    use near_sdk::test_utils::{VMContextBuilder,get_created_receipts};
+    use near_sdk::{testing_env, MockedBlockchain, VMContext};
+    
+    use regex::Regex;
+
+    fn get_context(is_view: bool) -> VMContext {
+        VMContextBuilder::new()
+            .signer_account_id("bob.near".try_into().unwrap())
+            .is_view(is_view)
+            .build()
+    }
+
+    #[test]
+    pub fn test_notify_mentions() {
+        let context = get_context(false);
+        testing_env!(context);
+        let text = "Mentioning @a.near and @bcdefg.near";
+        notify_mentions(text, 2);
+        let receipts = get_created_receipts();
+        assert_eq!(1, receipts.len());
+
+        let receipt = receipts.get(0).unwrap();
+        let receipt_str = format!("{:?}", receipt);
+        let re = Regex::new(r#"method_name: (\[[^\]]*\]), args: (\[[^\]]*\])"#).unwrap();
+
+        // Extract the method_name and args values
+        for cap in re.captures_iter(&receipt_str) {
+            let method_name = &cap[1];
+
+            let args = &cap[2];
+
+            let method_name = method_name.trim_start_matches('[').trim_end_matches(']').split(", ").map(|s| s.parse().unwrap()).collect::<Vec<u8>>();
+            let method_name = String::from_utf8(method_name).expect("Failed to convert method_name to String");
+
+            assert_eq!("set", method_name);
+
+            let args = args.trim_start_matches('[').trim_end_matches(']').split(", ").map(|s| s.parse().unwrap()).collect::<Vec<u8>>();
+            let args = String::from_utf8(args).expect("Failed to convert args to String");
+
+            assert_eq!("{\"data\":{\"bob.near\":{\"index\":{\"notify\":\"[{\\\"key\\\":\\\"a.near\\\",\\\"value\\\":{\\\"type\\\":\\\"devgovgigs/mention\\\",\\\"post\\\":2}},{\\\"key\\\":\\\"bcdefg.near\\\",\\\"value\\\":{\\\"type\\\":\\\"devgovgigs/mention\\\",\\\"post\\\":2}}]\"}}}}", args);
+        }
+    }
+
+    #[test]
+    pub fn test_no_mentions() {
+        let context = get_context(false);
+        testing_env!(context);
+        let text = "Not mentioning anyone";
+        notify_mentions(text, 2);
+        assert_eq!(0, get_created_receipts().len());
+    }
+}
