47de9d24a8bbab3caa3f2a023143e59c3975ee2d
Carina Akaia 🦾🦊🪐🚀 root.akaia( .near | .eth ) <carina@dmail.ai>
2023-06-15 22:41:36 +0400

    fix: Remove deprecated fields

--- src/community.rs
+++ src/community.rs
@@ -29,8 +29,6 @@ pub struct Community {
     pub sponsorship: Option<bool>,
     pub wiki1: Option<WikiPage>,
     pub wiki2: Option<WikiPage>,
-    pub overview_id: Option<u8>,
-    pub events_id: Option<u8>,
 }
 
 #[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize, Clone)]
