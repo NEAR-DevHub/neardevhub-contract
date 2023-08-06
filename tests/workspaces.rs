af7fc9fec780522d758412050486996c761b7fc9
Jaswinder Singh <jassmultani2@gmail.com>
2023-08-06 17:26:27 +0530

    added helper function for compile_project, finished test case.

--- tests/workspaces.rs
+++ tests/workspaces.rs
@@ -1,25 +1,28 @@
-// macro allowing us to convert args into JSON bytes to be read by the contract.
+use near_units::parse_near;
 use serde_json::json;
-use workspaces::{Account, AccountId, BlockHeight, Contract, Worker};
-use near_units::{parse_gas, parse_near};
-use workspaces::network::Sandbox;
-
+use std::env;
+use std::process::Command;
+use workspaces::AccountId;
 
 #[tokio::test]
-async fn test2() -> anyhow::Result<()> {
+async fn test_deploy_contract_self_upgrade() -> anyhow::Result<()> {
     const CONTRACT_ACCOUNT: &str = "devgovgigs.near";
     let worker = workspaces::sandbox().await?;
 
     let mainnet = workspaces::mainnet_archival().await?;
     let contract_id: AccountId = CONTRACT_ACCOUNT.parse()?;
 
+    //To use block height if needed
+    //const BLOCK_HEIGHT: BlockHeight = 97416242;
+
     let contract = worker
         .import_contract(&contract_id, &mainnet)
         .initial_balance(parse_near!("1000 N"))
+        //.block_height(BLOCK_HEIGHT)
         .transact()
         .await?;
-    
-    println!("{}",format!("{:?}", contract));
+
+    println!("{}", format!("{:?}", contract));
 
     let outcome = contract
         .call("new")
@@ -28,25 +31,61 @@ async fn test2() -> anyhow::Result<()> {
         .await?;
 
     assert!(outcome.is_success());
-    assert!(format!("{:?}", outcome)
-    .contains("Migrated to version:"));
+    assert!(format!("{:?}", outcome).contains("Migrated to version:"));
 
     println!("Init outcome: {:#?}", outcome);
 
-    let current_wasm = workspaces::compile_project("./").await?;
-    let self_upgrade = contract.call("unsafe_self_upgrade")
-                       .args(current_wasm)
-                       .max_gas()
-                       .transact()
-                       .await?;
-    
-    println!("Unsafe self upgrade: {:#?}", self_upgrade);
-    assert!(self_upgrade.is_success());
+    //compile the current code
 
-    //assert!(contract1.is_success());
+    // non-working function from workspaces, needs more debugging.
+    //let current_wasm = workspaces::compile_project("./").await?;
+
+    //tempororary solution using cargo build
+    //checkout comment here - https://github.com/near/neardevhub-contract/pull/46#issuecomment-1666830434
+    let result = compile_project("./").await;
+    assert!(result.is_ok(), "Compilation failed with error: {:?}", result.err().unwrap());
+
+    let wasm = std::fs::read("./target/wasm32-unknown-unknown/release/devgovgigs.wasm")?;
+
+    let self_upgrade = contract.call("unsafe_self_upgrade").args(wasm).max_gas().transact().await?;
+
+    println!("Unsafe self upgrade: {:#?}", self_upgrade);
+
+    //check if upgrade was success
+    assert!(self_upgrade.is_success());
+    if format!("{:?}", self_upgrade).contains("needs-migration") {
+        let migrate =
+            contract.call("unsafe_migrate").args_json(json!({})).max_gas().transact().await?;
+        assert!(format!("{:?}", migrate).contains("Migration done."));
+    } else {
+        assert!(format!("{:?}", self_upgrade).contains("Migration done."));
+    }
 
     Ok(())
+}
 
-    //do unsafe migrate
-    // do check some empty returning function // check the migrations video. 
+pub async fn compile_project(project_path: &str) -> Result<(), std::io::Error> {
+    // Set RUSTFLAGS environment variable
+    env::set_var("RUSTFLAGS", "-C link-arg=-s");
+    // Change current directory to parent directory
+    env::set_current_dir(project_path)?;
+
+    // Run the command
+    let output = Command::new("cargo")
+        .arg("build")
+        .arg("--target")
+        .arg("wasm32-unknown-unknown")
+        .arg("--release")
+        .output()
+        .expect("failed to execute process");
+
+    if output.status.success() {
+        let text = String::from_utf8(output.stdout).unwrap();
+        println!("Build successful: {}", text);
+        Ok(())
+    } else {
+        let err = String::from_utf8(output.stderr).unwrap();
+        println!("Build failed: {}", err);
+        Err(std::io::Error::new(std::io::ErrorKind::Other, "Build failed"))
+    }
 }
