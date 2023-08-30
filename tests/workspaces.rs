2dc3041ee6b951be58fbc83712c7c8b4fb71e60f
Jaswinder Singh <jassmultani2@gmail.com>
2023-08-30 22:28:00 +0530

    wip: adding posts and checking self_upgrade

--- tests/workspaces.rs
+++ tests/workspaces.rs
@@ -3,13 +3,54 @@ use serde_json::json;
 use std::env;
 use std::process::Command;
 use workspaces::AccountId;
+use workspaces::BlockHeight;
 
 #[tokio::test]
 async fn test_deploy_contract_self_upgrade() -> anyhow::Result<()> {
     const CONTRACT_ACCOUNT: &str = "devgovgigs.near";
+    const NEAR_SOCIAL: &str = "social.near";
     let worker = workspaces::sandbox().await?;
 
     let mainnet = workspaces::mainnet_archival().await?;
+
+    //near social deployment
+    let near_social_id: AccountId = NEAR_SOCIAL.parse()?;
+
+    //To use block height if needed
+    const BLOCK_HEIGHT: BlockHeight = 98902482;
+
+    let near_social = worker
+        .import_contract(&near_social_id, &mainnet)
+        .initial_balance(parse_near!("10000 N"))
+        //.block_height(BLOCK_HEIGHT)
+        .transact()
+        .await?;
+
+    println!("{}", format!("{:?}", near_social));
+
+    // let near_social_outcome = near_social
+    //     .call("new")
+    //     .args_json(json!({}))
+    //     .transact() // note: we use the contract's keys here to sign the transaction
+    //     .await?;
+
+    // println!("Near social outcome: {:#?}", near_social_outcome);
+
+    near_social.call("new").transact().await?.into_result()?;
+    let near_social_outcome = near_social
+        .as_account()
+        .call(near_social.id(), "set_status")
+        .args_json(json!({
+            "status": "Live"
+        }))
+        .transact()
+        .await?
+        .into_result()?;
+
+    println!("Near social outcome: {:#?}", near_social_outcome);
+
+
+    // devhub contract deployment
     let contract_id: AccountId = CONTRACT_ACCOUNT.parse()?;
 
     //To use block height if needed
@@ -18,7 +59,7 @@ async fn test_deploy_contract_self_upgrade() -> anyhow::Result<()> {
     let contract = worker
         .import_contract(&contract_id, &mainnet)
         .initial_balance(parse_near!("1000 N"))
-        //.block_height(BLOCK_HEIGHT)
+        .block_height(BLOCK_HEIGHT)
         .transact()
         .await?;
 
@@ -35,12 +76,30 @@ async fn test_deploy_contract_self_upgrade() -> anyhow::Result<()> {
 
     println!("Init outcome: {:#?}", outcome);
 
+    let deposit_amount = near_units::parse_near!("0.1");
+    let idea_post = contract
+        .call("add_post")
+        .args_json(json!({
+            "parent_id": null,
+            "labels": [],
+            "body": {
+            "name": "This is a test idea.",
+            "description": "This is a test description.",
+            "post_type": "Idea",
+            "idea_version": "V1"
+        }
+        }))
+        .deposit(deposit_amount)
+        .transact()
+        .await?;
+
+    assert!(idea_post.is_success());
+    println!("Idea Post: {:#?}", idea_post);
     //compile the current code
-
     // non-working function from workspaces, needs more debugging.
-    //let current_wasm = workspaces::compile_project("./").await?;
+    //let wasm = workspaces::compile_project("./").await?;
 
-    //tempororary solution using cargo build
+    //temporary solution using cargo build
     //checkout comment here - https://github.com/near/neardevhub-contract/pull/46#issuecomment-1666830434
     let result = compile_project("./").await;
     assert!(result.is_ok(), "Compilation failed with error: {:?}", result.err().unwrap());
