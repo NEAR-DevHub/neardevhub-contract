b4b4388a6a73afd0d70a044c607ad2785ce27d3e
Vlad Frolov <frolvlad@gmail.com>
2023-09-05 14:52:26 +0200

    feat: use `insta` crate for snapshot testing

--- tests/workspaces.rs
+++ tests/workspaces.rs
@@ -7,155 +7,142 @@ const NEAR_SOCIAL: &str = "social.near";
 
 #[tokio::test]
 async fn test_deploy_contract_self_upgrade() -> anyhow::Result<()> {
-    //Test Flow
-    // 1.Deploy devhub and near social contract on sandbox
+    // Test Flow:
+    // 1. Deploy devhub and near social contract on sandbox
     // 2. Add all kinds of posts and add a community.
     // 3. Upgrade the contract.
     // 4. Get all the posts and community and check if migration was succesfull.
 
     // Initialize the devhub and near social contract on chain,
-    //contract is devhub contract instance.
+    // contract is devhub contract instance.
     let contract = init_contracts().await?;
 
     let deposit_amount = near_units::parse_near!("0.1");
 
-    //Add Posts
-    let idea_post = contract
+    // Add Posts
+    let add_idea_post = contract
         .call("add_post")
         .args_json(json!({
             "parent_id": null,
             "labels": [],
             "body": {
-            "name": "This is a test idea.",
-            "description": "This is a test description.",
-            "post_type": "Idea",
-            "idea_version": "V1"
-        }
+                "name": "This is a test idea.",
+                "description": "This is a test description.",
+                "post_type": "Idea",
+                "idea_version": "V1"
+            }
         }))
         .deposit(deposit_amount)
         .transact()
         .await?;
+    assert!(add_idea_post.is_success());
 
-    assert!(idea_post.is_success());
-
-    let submission_post = contract
+    let add_submission_post = contract
         .call("add_post")
         .args_json(json!({
             "parent_id": null,
             "labels": [],
             "body": {
-            "name": "Solution Test",
-            "description": "###### Requested amount: 100 NEAR\n###### Requested sponsor: @neardevgov.near\nThis is a test submission. ",
-            "post_type": "Submission",
-            "submission_version": "V1"
-        }
+                "name": "Solution Test",
+                "description": "###### Requested amount: 100 NEAR\n###### Requested sponsor: @neardevgov.near\nThis is a test submission. ",
+                "post_type": "Submission",
+                "submission_version": "V1"
+            }
         }))
         .deposit(deposit_amount)
         .max_gas()
         .transact()
         .await?;
+    assert!(add_submission_post.is_success());
 
-    assert!(submission_post.is_success());
-
-    let comment = contract
+    let add_comment_post = contract
         .call("add_post")
         .args_json(json!({
             "parent_id": 0,
             "labels": [],
             "body": {
-            "description": "This is test Comment.",
-            "comment_version": "V2",
-            "post_type": "Comment"
+                "description": "This is test Comment.",
+                "comment_version": "V2",
+                "post_type": "Comment"
             }
         }))
         .deposit(deposit_amount)
         .max_gas()
         .transact()
         .await?;
+    assert!(add_comment_post.is_success());
 
-    assert!(comment.is_success());
-
-    let attestation = contract
+    let add_attestation_post = contract
         .call("add_post")
         .args_json(json!({
             "parent_id": 1,
             "labels": [],
             "body": {
-            "name": "Attestation",
-            "description": "Description",
-            "attestation_version": "V1",
-            "post_type": "Attestation"
+                "name": "Attestation",
+                "description": "Description",
+                "attestation_version": "V1",
+                "post_type": "Attestation"
             }
         }))
         .deposit(deposit_amount)
         .max_gas()
         .transact()
         .await?;
+    assert!(add_attestation_post.is_success());
 
-    assert!(attestation.is_success());
-
-    let sponsorship = contract
+    let add_sponsorship_post = contract
         .call("add_post")
         .args_json(json!({
             "parent_id": 1,
             "labels": [],
             "body": {
-            "name": "Contributor fellowship",
-            "description": "Funding approved",
-            "amount": "1000",
-            "sponsorship_token": "Near",
-            "supervisor": "john.near",
-            "sponsorship_version": "V1",
-            "post_type": "Sponsorship"
+                "name": "Contributor fellowship",
+                "description": "Funding approved",
+                "amount": "1000",
+                "sponsorship_token": "Near",
+                "supervisor": "john.near",
+                "sponsorship_version": "V1",
+                "post_type": "Sponsorship"
             }
         }))
         .deposit(deposit_amount)
         .max_gas()
         .transact()
         .await?;
+    assert!(add_sponsorship_post.is_success());
 
-    assert!(sponsorship.is_success());
-
-    //Add a community
-    let community = contract
+    // Add a community
+    let create_community = contract
         .call("create_community")
         .args_json(json!({
-                "inputs": {
-                  "handle": "gotham",
-                  "name": "Gotham",
-                  "tag": "some",
-                  "description": "This is a test community.",
-                  "bio_markdown": "This is a sample text about your community.\nYou can change it on the community configuration page.",
-                  "logo_url": "https://ipfs.near.social/ipfs/bafkreibysr2mkwhb4j36h2t7mqwhynqdy4vzjfygfkfg65kuspd2bawauu",
-                  "banner_url": "https://ipfs.near.social/ipfs/bafkreic4xgorjt6ha5z4s5e3hscjqrowe5ahd7hlfc5p4hb6kdfp6prgy4"
-                }
+            "inputs": {
+                "handle": "gotham",
+                "name": "Gotham",
+                "tag": "some",
+                "description": "This is a test community.",
+                "bio_markdown": "This is a sample text about your community.\nYou can change it on the community configuration page.",
+                "logo_url": "https://ipfs.near.social/ipfs/bafkreibysr2mkwhb4j36h2t7mqwhynqdy4vzjfygfkfg65kuspd2bawauu",
+                "banner_url": "https://ipfs.near.social/ipfs/bafkreic4xgorjt6ha5z4s5e3hscjqrowe5ahd7hlfc5p4hb6kdfp6prgy4"
+            }
         }))
         .max_gas()
         .transact()
         .await?;
+    assert!(create_community.is_success());
 
-    println!("{}", format!("{:?}", community));
-
-    assert!(community.is_success());
-
-    //Call self upgrade with current branch code
-    //compile the current code
+    // Call self upgrade with current branch code
+    // compile the current code
     let wasm = workspaces::compile_project("./").await?;
 
-    let self_upgrade = contract.call("unsafe_self_upgrade").args(wasm).max_gas().transact().await?;
+    let mut contract_upgrade_result =
+        contract.call("unsafe_self_upgrade").args(wasm).max_gas().transact().await?;
 
-    assert!(self_upgrade.is_success());
-
-    //needs migration or not
-    if format!("{:?}", self_upgrade).contains("needs-migration") {
-        let migrate =
+    while contract_upgrade_result.json::<String>()? == "needs-migration" {
+        contract_upgrade_result =
             contract.call("unsafe_migrate").args_json(json!({})).max_gas().transact().await?;
-        assert!(format!("{:?}", migrate).contains("Migration done."));
-    } else {
-        assert!(format!("{:?}", self_upgrade).contains("Migration done."));
     }
 
-    let get_idea: serde_json::Value = contract
+    let get_idea_post: serde_json::Value = contract
         .call("get_post")
         .args_json(json!({
             "post_id" : 0
@@ -163,10 +150,9 @@ async fn test_deploy_contract_self_upgrade() -> anyhow::Result<()> {
         .view()
         .await?
         .json()?;
+    insta::assert_json_snapshot!(get_idea_post, {".snapshot.timestamp" => "[timestamp]"});
 
-    assert!(get_idea.to_string().contains("This is a test idea."));
-
-    let get_comment: serde_json::Value = contract
+    let get_comment_posts: serde_json::Value = contract
         .call("get_posts")
         .args_json(json!({
             "parent_id" : 0
@@ -174,10 +160,9 @@ async fn test_deploy_contract_self_upgrade() -> anyhow::Result<()> {
         .view()
         .await?
         .json()?;
+    insta::assert_json_snapshot!(get_comment_posts, {"[].snapshot.timestamp" => "[timestamp]"});
 
-    assert!(get_comment.to_string().contains("This is test Comment."));
-
-    let get_submission: serde_json::Value = contract
+    let get_submission_post: serde_json::Value = contract
         .call("get_post")
         .args_json(json!({
             "post_id" : 1
@@ -185,10 +170,9 @@ async fn test_deploy_contract_self_upgrade() -> anyhow::Result<()> {
         .view()
         .await?
         .json()?;
+    insta::assert_json_snapshot!(get_submission_post, {".snapshot.timestamp" => "[timestamp]"});
 
-    assert!(get_submission.to_string().contains("This is a test submission"));
-
-    let get_attestation_sponsorship: serde_json::Value = contract
+    let get_attestation_sponsorship_posts: serde_json::Value = contract
         .call("get_posts")
         .args_json(json!({
             "parent_id" : 1
@@ -196,9 +180,7 @@ async fn test_deploy_contract_self_upgrade() -> anyhow::Result<()> {
         .view()
         .await?
         .json()?;
-
-    assert!(get_attestation_sponsorship.to_string().contains("Attestation"));
-    assert!(get_attestation_sponsorship.to_string().contains("Sponsorship"));
+    insta::assert_json_snapshot!(get_attestation_sponsorship_posts, {"[].snapshot.timestamp" => "[timestamp]"});
 
     let get_community: serde_json::Value = contract
         .call("get_community")
@@ -208,8 +190,7 @@ async fn test_deploy_contract_self_upgrade() -> anyhow::Result<()> {
         .view()
         .await?
         .json()?;
-
-    assert!(get_community.to_string().contains("This is a test community."));
+    insta::assert_json_snapshot!(get_community);
 
     Ok(())
 }
