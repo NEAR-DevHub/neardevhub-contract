fca6fc48678b75292ba3b4b390eb0c56585917f8
Vlad Frolov <frolvlad@gmail.com>
2023-09-11 16:56:53 +0200

    fix: Renamed 'Near' to 'NEAR' in the SponsorshipToken to align with the front-end and the NEAR branding

--- src/post/sponsorship.rs
+++ src/post/sponsorship.rs
@@ -5,25 +5,13 @@ use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::{AccountId, Balance, Timestamp};
 use std::collections::HashSet;
-use std::str::FromStr;
 
 #[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize, Clone)]
 #[serde(crate = "near_sdk::serde")]
 pub enum SponsorshipToken {
-    Near,
+    NEAR,
     NEP141 { address: AccountId },
     USD,
-}
-
-impl FromStr for SponsorshipToken {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s.to_lowercase().as_str() {
-            "near" => Ok(Self::Near),
-            _ => Ok(Self::NEP141 { address: s.to_string() }),
-        }
-    }
 }
 
 #[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize, Clone)]
