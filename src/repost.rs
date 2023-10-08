b06629b7694a05c12e7bd4ba570113b12dde073e
Owen <124691791+PiVortex@users.noreply.github.com>
2023-10-08 22:15:31 +0100

    refactor: Update to near-sdk-rs 4.1.1 (#63)

--- src/repost.rs
+++ src/repost.rs
@@ -3,6 +3,7 @@ use crate::social_db::{ext_social_db, SOCIAL_DB};
 use near_sdk::serde_json::json;
 use near_sdk::{env, AccountId, Promise};
 
+
 fn repost_internal(post: Post, contract_address: AccountId) -> near_sdk::serde_json::Value {
     let post_link = format!("https://near.social/#/devgovgigs.near/widget/Post?id={}", post.id);
     let title = match post.snapshot.body.clone() {
@@ -32,13 +33,12 @@ fn repost_internal(post: Post, contract_address: AccountId) -> near_sdk::serde_j
     let main_value = json!({
         "type": "md",
         "text": text
-    })
-    .to_string();
+    });
 
     json!({
         contract_address: {
             "post": {
-                "main": main_value,
+                "main": main_value.to_string(),
             },
             "index": {
                 "post": "{\"key\":\"main\",\"value\":{\"type\":\"md\"}}",
@@ -48,16 +48,16 @@ fn repost_internal(post: Post, contract_address: AccountId) -> near_sdk::serde_j
 }
 
 pub fn repost(post: Post) -> Promise {
-    ext_social_db::set(
-        repost_internal(post, env::current_account_id()),
-        &SOCIAL_DB,
-        env::attached_deposit(),
-        env::prepaid_gas() / 3,
-    )
+    ext_social_db::ext(SOCIAL_DB.parse().unwrap())
+        .with_static_gas(env::prepaid_gas() / 3)
+        .with_attached_deposit(env::attached_deposit())
+        .set(repost_internal(post, env::current_account_id()))
 }
 
 #[cfg(test)]
 mod tests {
+    use std::convert::TryInto;
+
     use crate::post::{IdeaV1, Post, PostBody, PostSnapshot, VersionedIdea};
     use crate::repost::repost_internal;
     use near_sdk::serde_json::json;
@@ -66,10 +66,10 @@ mod tests {
     pub fn check_formatting() {
         let post = Post {
             id: 0,
-            author_id: "neardevgov.near".to_string(),
+            author_id: "neardevgov.near".parse().unwrap(),
             likes: Default::default(),
             snapshot: PostSnapshot {
-                editor_id: "neardevgov.near".to_string(),
+                editor_id: "neardevgov.near".parse().unwrap(),
                 timestamp: 0,
                 labels: Default::default(),
                 body: PostBody::Idea(VersionedIdea::V1(IdeaV1 { name: "A call for Zero Knowledge Work Group members!".to_string(), description: "We are excited to create a more formal Zero Knowledge Work Group (WG) to oversee official decisions on Zero Knowledge proposals. We’re looking for 3-7 experts to participate. Reply to the post if you’re interested in becoming a work group member.".to_string() })),
@@ -77,7 +77,7 @@ mod tests {
             snapshot_history: vec![],
         };
 
-        let call_args = repost_internal(post, "devgovgigs.near".to_string());
+        let call_args = repost_internal(post, "devgovgigs.near".parse().unwrap());
         let expected = json!({
             "devgovgigs.near": {
                 "post": {
