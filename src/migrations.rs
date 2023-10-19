1783639790fb7104616597088f90456a5cedda16
Carina.Akaia.near <carina@dmail.ai>
2023-10-19 17:25:32 +0400

    wip: Rollback migrations

--- src/migrations.rs
+++ src/migrations.rs
@@ -21,7 +21,6 @@ impl Contract {
     fn unsafe_add_acl() {
         let ContractV1 { posts, post_to_parent, post_to_children, label_to_posts } =
             env::state_read().unwrap();
-
         env::state_write(&ContractV2 {
             posts,
             post_to_parent,
@@ -116,7 +115,6 @@ impl Contract {
             access_control,
             authors,
         } = env::state_read().unwrap();
-
         env::state_write(&ContractV4 {
             posts,
             post_to_parent,
@@ -153,7 +151,6 @@ impl Contract {
             authors,
             communities,
         } = env::state_read().unwrap();
-
         env::state_write(&ContractV5 {
             posts,
             post_to_parent,
@@ -401,58 +398,6 @@ pub struct ContractV7 {
     pub featured_communities: Vec<FeaturedCommunity>,
 }
 
-// From ContractV7 to ContractV8
-#[near_bindgen]
-impl Contract {
-    fn unsafe_migrate_solution_posts(start: u64, end: u64) -> StateVersion {
-        let ContractV7 {
-            posts,
-            post_to_parent,
-            post_to_children,
-            label_to_posts,
-            access_control,
-            authors,
-            communities,
-            featured_communities,
-        } = env::state_read().unwrap();
-
-        let mut posts = posts;
-        let total = posts.len();
-        let end = min(total, end);
-
-        for post_index in start..end {
-            if let Some(versioned_post) = posts.get(post_index) {
-                posts.replace(post_index, &versioned_post.into());
-            }
-        }
-
-        env::state_write(&ContractV8 {
-            posts,
-            post_to_parent,
-            post_to_children,
-            label_to_posts,
-            access_control,
-            authors,
-            communities,
-            featured_communities,
-        });
-
-        StateVersion::V8 { done: end == total, migrated_count: end }
-    }
-}
-
-#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
-pub struct ContractV8 {
-    pub posts: Vector<VersionedPost>,
-    pub post_to_parent: LookupMap<PostId, PostId>,
-    pub post_to_children: LookupMap<PostId, Vec<PostId>>,
-    pub label_to_posts: UnorderedMap<String, HashSet<PostId>>,
-    pub access_control: AccessControl,
-    pub authors: UnorderedMap<AccountId, HashSet<PostId>>,
-    pub communities: UnorderedMap<String, CommunityV3>,
-    pub featured_communities: Vec<FeaturedCommunity>,
-}
-
 #[derive(BorshDeserialize, BorshSerialize, Debug)]
 pub(crate) enum StateVersion {
     V1,
@@ -462,7 +407,6 @@ pub(crate) enum StateVersion {
     V5,
     V6,
     V7,
-    V8 { done: bool, migrated_count: u64 },
 }
 
 const VERSION_KEY: &[u8] = b"VERSION";
@@ -511,60 +455,40 @@ impl Contract {
         near_sdk::assert_self();
         let current_version = state_version_read();
         near_sdk::log!("Migrating from version: {:?}", current_version);
-
         match current_version {
             StateVersion::V1 => {
                 Contract::unsafe_add_acl();
                 state_version_write(&StateVersion::V2);
             }
-
             StateVersion::V2 => {
                 Contract::unsafe_add_post_authors();
                 state_version_write(&StateVersion::V3 { done: false, migrated_count: 0 })
             }
-
             StateVersion::V3 { done: false, migrated_count } => {
                 let new_version =
                     Contract::unsafe_insert_old_post_authors(migrated_count, migrated_count + 100);
                 state_version_write(&new_version);
             }
-
             StateVersion::V3 { done: true, migrated_count: _ } => {
                 Contract::unsafe_add_communities();
                 state_version_write(&StateVersion::V4);
             }
-
             StateVersion::V4 => {
                 Contract::unsafe_add_featured_communities();
                 state_version_write(&StateVersion::V5);
             }
-
             StateVersion::V5 => {
                 Contract::unsafe_multiple_telegrams();
                 state_version_write(&StateVersion::V6);
             }
-
             StateVersion::V6 => {
                 Contract::unsafe_add_board_and_feature_flags();
                 state_version_write(&StateVersion::V7);
             }
-
-            StateVersion::V7 => {
-                state_version_write(&StateVersion::V8 { done: false, migrated_count: 0 })
-            }
-
-            StateVersion::V8 { done: false, migrated_count } => {
-                state_version_write(&Contract::unsafe_migrate_solution_posts(
-                    migrated_count,
-                    migrated_count + 100,
-                ));
-            }
-
             _ => {
                 return Contract::migration_done();
             }
         }
-
         Contract::needs_migration();
     }
 }
