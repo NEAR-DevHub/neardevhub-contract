adadfdc9d71dd62fbeb3dd59b46db7854eceba70
T guntenaar <thomas@guntenaar.org>
2023-10-19 13:56:48 -0500

    feat: introduced community add-ons (#61)

--- src/community/mod.rs
+++ src/community/mod.rs
@@ -4,6 +4,8 @@ use near_sdk::{env, require, AccountId};
 
 pub type CommunityHandle = String;
 
+pub type AddOnId = String;
+
 #[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize, Clone)]
 #[serde(crate = "near_sdk::serde")]
 pub struct CommunityInputs {
@@ -45,7 +47,55 @@ pub struct WikiPage {
     content_markdown: String,
 }
 
-#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize, Clone)]
+#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize, Clone, PartialEq, Debug)]
+#[serde(crate = "near_sdk::serde")]
+pub struct CommunityAddOn {
+    pub id: String,
+    pub addon_id: AddOnId,
+    pub display_name: String,
+    pub enabled: bool,
+    pub parameters: String,
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize, Clone, PartialEq, Debug)]
+#[serde(crate = "near_sdk::serde")]
+pub struct AddOn {
+    pub id: AddOnId,
+    pub title: String,
+    pub description: String,
+    pub icon: String,
+    // The path to the view on the community page
+    pub view_widget: String,
+    // The path to the view on the community configuration page
+    pub configurator_widget: String,
+}
+
+impl AddOn {
+    pub fn validate(&self) {
+        if !matches!(self.id.chars().count(), 3..=120) {
+            panic!("Add-on id must contain 3 to 120 characters");
+        }
+
+        if !matches!(self.title.chars().count(), 3..=120) {
+            panic!("Add-on title must contain 3 to 120 characters");
+        }
+
+        if !matches!(self.description.chars().count(), 3..=120) {
+            panic!("Add-on description must contain 3 to 120 characters");
+        }
+        if !matches!(self.view_widget.chars().count(), 6..=240) {
+            panic!("Add-on viewer must contain 6 to 240 characters");
+        }
+        if !matches!(self.configurator_widget.chars().count(), 0..=240) {
+            panic!("Add-on configurator must contain 0 to 240 characters");
+        }
+        if !matches!(self.icon.chars().count(), 6..=60) {
+            panic!("Add-on icon must contain 6 to 60 characters");
+        }
+    }
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
 #[serde(crate = "near_sdk::serde")]
 pub struct Community {
     pub admins: Vec<AccountId>,
@@ -67,6 +117,7 @@ pub struct Community {
     pub wiki1: Option<WikiPage>,
     pub wiki2: Option<WikiPage>,
     pub features: CommunityFeatureFlags,
+    pub addons: Vec<CommunityAddOn>,
 }
 
 #[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize, Clone)]
@@ -108,6 +159,18 @@ impl Community {
                 ),
             "Community bio must contain 3 to 200 characters"
         );
+    }
+
+    pub fn add_addon(&mut self, addon_config: CommunityAddOn) {
+        self.addons.push(addon_config);
+    }
+
+    pub fn remove_addon(&mut self, addon_to_remove: CommunityAddOn) {
+        self.addons.retain(|addon| addon != &addon_to_remove);
+    }
+
+    pub fn set_addons(&mut self, addons: Vec<CommunityAddOn>) {
+        self.addons = addons;
     }
 
     pub fn set_default_admin(&mut self) {
--- src/migrations.rs
+++ src/migrations.rs
@@ -398,6 +398,109 @@ pub struct ContractV7 {
     pub featured_communities: Vec<FeaturedCommunity>,
 }
 
+// From ContractV7 to ContractV8
+#[near_bindgen]
+impl Contract {
+    fn unsafe_add_community_addons() {
+        let ContractV7 {
+            posts,
+            post_to_parent,
+            post_to_children,
+            label_to_posts,
+            access_control,
+            authors,
+            mut communities,
+            featured_communities,
+        } = env::state_read().unwrap();
+
+        let migrated_communities: Vec<(String, CommunityV4)> = communities
+            .iter()
+            .map(|(community_handle, community)| {
+                (
+                    community_handle,
+                    CommunityV4 {
+                        admins: community.admins,
+                        handle: community.handle,
+                        name: community.name,
+                        tag: community.tag,
+                        description: community.description,
+                        logo_url: community.logo_url,
+                        banner_url: community.banner_url,
+                        bio_markdown: community.bio_markdown,
+                        github_handle: community.github_handle,
+                        telegram_handle: community.telegram_handle,
+                        twitter_handle: community.twitter_handle,
+                        website_url: community.website_url,
+                        github: community.github,
+                        board: None,
+                        wiki1: community.wiki1,
+                        wiki2: community.wiki2,
+                        features: community.features,
+                        addons: Vec::new(),
+                    },
+                )
+            })
+            .collect();
+
+        communities.clear();
+
+        let mut communities_new = UnorderedMap::new(StorageKey::Communities);
+
+        for (k, v) in migrated_communities {
+            communities_new.insert(&k, &v);
+        }
+
+        env::state_write(&ContractV8 {
+            posts,
+            post_to_parent,
+            post_to_children,
+            label_to_posts,
+            access_control,
+            authors,
+            communities: communities_new,
+            featured_communities,
+            available_addons: UnorderedMap::new(StorageKey::AddOns),
+        });
+    }
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct CommunityV4 {
+    pub admins: Vec<AccountId>,
+    pub handle: CommunityHandle,
+    pub name: String,
+    pub tag: String,
+    pub description: String,
+    pub logo_url: String,
+    pub banner_url: String,
+    pub bio_markdown: Option<String>,
+    pub github_handle: Option<String>,
+    pub telegram_handle: Vec<String>,
+    pub twitter_handle: Option<String>,
+    pub website_url: Option<String>,
+    /// JSON string of github board configuration
+    pub github: Option<String>,
+    /// JSON string of kanban board configuration
+    pub board: Option<String>,
+    pub wiki1: Option<WikiPage>,
+    pub wiki2: Option<WikiPage>,
+    pub features: CommunityFeatureFlags,
+    pub addons: Vec<CommunityAddOn>,
+}
+
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
+pub struct ContractV8 {
+    pub posts: Vector<VersionedPost>,
+    pub post_to_parent: LookupMap<PostId, PostId>,
+    pub post_to_children: LookupMap<PostId, Vec<PostId>>,
+    pub label_to_posts: UnorderedMap<String, HashSet<PostId>>,
+    pub access_control: AccessControl,
+    pub authors: UnorderedMap<AccountId, HashSet<PostId>>,
+    pub communities: UnorderedMap<String, CommunityV4>,
+    pub featured_communities: Vec<FeaturedCommunity>,
+    pub available_addons: UnorderedMap<AddOnId, AddOn>,
+}
+
 #[derive(BorshDeserialize, BorshSerialize, Debug)]
 pub(crate) enum StateVersion {
     V1,
@@ -407,6 +510,7 @@ pub(crate) enum StateVersion {
     V5,
     V6,
     V7,
+    V8,
 }
 
 const VERSION_KEY: &[u8] = b"VERSION";
@@ -484,6 +588,10 @@ impl Contract {
             StateVersion::V6 => {
                 Contract::unsafe_add_board_and_feature_flags();
                 state_version_write(&StateVersion::V7);
+            }
+            StateVersion::V7 => {
+                Contract::unsafe_add_community_addons();
+                state_version_write(&StateVersion::V8);
             }
             _ => {
                 return Contract::migration_done();
