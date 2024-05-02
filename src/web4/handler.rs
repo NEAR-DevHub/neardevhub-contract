92265f624b035a93f5bd7d52f5e2443b751568f4
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-05-02 18:17:25 +0200

    top bar according to proposed figma design

--- src/web4/handler.rs
+++ src/web4/handler.rs
@@ -81,18 +81,30 @@ pub fn web4_get(contract: &Contract, request: Web4Request) -> Web4Response {
     <meta name="twitter:image" content="{image}">
     <script src="https://ipfs.web4.near.page/ipfs/bafybeic6aeztkdlthx5uwehltxmn5i6owm47b7b2jxbbpwmydv2mwxdfca/main.794b6347ae264789bc61.bundle.js"></script>
     <script src="https://ipfs.web4.near.page/ipfs/bafybeic6aeztkdlthx5uwehltxmn5i6owm47b7b2jxbbpwmydv2mwxdfca/runtime.25b143da327a5371509f.bundle.js"></script>
+    <style>
+        @media screen and (max-width: 600px) {{
+            .gatewaylinks .nav-link {{
+                padding-top: 0px!important;
+                padding-bottom: 0px!important;
+                margin: 0px;
+            }}
+            .gatewaylinks img {{
+                height: 30px;
+            }}
+        }}
+    </style>
 </head>
 <body>
-<nav class="navbar navbar-expand-sm navbar-light bg-light" style="display: flex; flex-wrap: nowrap; padding-left: 5px; padding-right: 5px;">
-    <a class="navbar-brand" href="/""><img src="https://i.near.social/magic/large/https://near.social/magic/img/account/devhub.near" style="width: 64px" /></a>
+<nav class="navbar navbar-expand-sm navbar-light bg-dark" style="display: flex; flex-wrap: nowrap; padding-left: 5px; padding-right: 5px; height: 73px; border-bottom: rgb(0, 236, 151) solid 5px;">
+    <a class="navbar-brand" href="/""><img src="https://i.near.social/magic/large/https://near.social/magic/img/account/devhub.near" style="height: 68px" /></a>
     <p class="nav-text" style="flex-grow: 1"></p>
-    <p class="nav-text">Choose your gateway</p>
-    <div class="navbar-nav">
-        <a href="https://near.org/{redirect_path}" class="btn btn-light btn-sm">
-            <img src="https://near.org/_next/static/media/near-logo.1416a213.svg" />
+    <p class="nav-text text-light" style="margin-top: 1rem; margin-right: 1rem">Choose your gateway</p>
+    <div class="navbar-nav gatewaylinks">
+        <a href="https://near.org/{redirect_path}" class="nav-link">
+            <img src="https://ipfs.web4.near.page/ipfs/bafybeia2ptgyoz7b6oxu3k57jmiras2pgigmw7a3cp6osjog67rndmf36y/nearorg.svg" />
         </a>
-        <a href="https://near.social/{redirect_path}" class="btn btn-dark btn-sm">
-            <img src="https://ipfs.web4.near.page/ipfs/bafybeieugu57nj5c6jhgtozvbyj6am3lh5baeb4uvjh3re26ihp7sjmnra/nearsocial.svg" />
+        <a href="https://near.social/{redirect_path}" class="nav-link">
+            <img src="https://ipfs.web4.near.page/ipfs/bafybeia2ptgyoz7b6oxu3k57jmiras2pgigmw7a3cp6osjog67rndmf36y/nearsocial.svg" />
         </a>
     </div>
 </nav>
