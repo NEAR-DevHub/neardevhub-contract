d0266a38d580504cfe6ee25260a6be94ecf9d3c5
Vadim Volodin <vad.e.volodin@gmail.com>
2024-05-20 17:47:00 +0800

    Update src/rfp/repost.rs
    Co-authored-by: Vlad Frolov <frolvlad@gmail.com>

--- src/rfp/repost.rs
+++ src/rfp/repost.rs
@@ -9,7 +9,7 @@ pub fn rfp_repost_text(rfp: RFP) -> String {
     let summary = body.summary;
 
     let text = format!(
-        "A new Request for Proposals is published.\n\n———\n\n**Title**: “{title}“\n\n**Summary**:\n\n{summary}\n\n———\n\nRead the full RFP and participate on [its page]({rfp_link})",
+        "A new Request for Proposals is published.\n\n———\n\n**Title**: “{title}“\n\n**Summary**:\n\n{summary}\n\n———\n\nRead the full RFP and participate [here]({rfp_link})",
         rfp_link = rfp_link,
         title = title,
         summary = summary,
