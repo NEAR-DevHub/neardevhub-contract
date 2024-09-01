75684df760cb681d06d694c4c7746fb8d0202c17
Peter Salomonsen <petersalomonsen@runbox.com>
2024-09-01 20:08:02 +0000

    latest cargo near in devcontainer, update swatinem/rust-cache build without docker

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
 
-(cd discussions && cargo near build)
-(cd community && cargo near build)
-(cd community-factory && cargo near build)
-cargo near build
+curl --proto '=https' --tlsv1.2 -LsSf https://github.com/near/cargo-near/releases/latest/download/cargo-near-installer.sh | sh
+(cd discussions && cargo near build --no-docker)
+(cd community && cargo near build --no-docker)
+(cd community-factory && cargo near build --no-docker)
+cargo near build --no-docker
