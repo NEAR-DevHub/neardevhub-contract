37f3821c420c60fd23bf043acb395c02adc46b75
Peter Salomonsen <petersalomonsen@runbox.com>
2024-09-08 19:32:42 +0000

    rust devcontainer feature instead of installing rust

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source $HOME/.cargo/env
+
+curl --proto '=https' --tlsv1.2 -LsSf https://github.com/near/near-cli-rs/releases/latest/download/near-cli-rs-installer.sh | sh
 
 curl --proto '=https' --tlsv1.2 -LsSf https://github.com/near/cargo-near/releases/latest/download/cargo-near-installer.sh | sh
 (cd discussions && cargo near build --no-docker)
