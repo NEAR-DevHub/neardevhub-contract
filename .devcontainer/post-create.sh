e5b5e448590cd1ea1b226dfe78470e14c88d5d62
dj8yf0μl <noreply@nowhere.org>
2024-09-11 14:25:09 +0300

    chore:  change back to ghcr.io/near/near-devcontainer:latest

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-curl --proto '=https' --tlsv1.2 -LsSf https://github.com/near/near-cli-rs/releases/latest/download/near-cli-rs-installer.sh | sh
-
-curl --proto '=https' --tlsv1.2 -LsSf https://github.com/near/cargo-near/releases/latest/download/cargo-near-installer.sh | sh
-
 (cd community-factory && cargo near build --no-docker)
 cargo near build --no-docker
