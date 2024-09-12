4d1b4e8752468a4cbe7a52fbf757296cd6f2d37f
dj8yf0μl <26653921+dj8yfo@users.noreply.github.com>
2024-09-12 18:11:25 +0300

    chore: create workspace (#150)
    - **chore: create workspace**
    - **chore: use repository.workspace = true in contracts**
    
    ---------
    
    Co-authored-by: dj8yf0μl <noreply@nowhere.org>

--- community-factory/build.rs
+++ community-factory/build.rs
@@ -2,9 +2,6 @@ use cargo_near_build::extended::BuildScriptOpts;
 
 fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     println!("cargo:warning={}", format!("`devhub-community-factory` build script working dir: {:?}", std::env::current_dir().expect("get current dir")));
-    // additional step required; can be removed when project moved to 
-    // workspaces usage, as all contracts will have a shared target 
-    std::fs::create_dir_all("../community/target")?;
 
     let opts = cargo_near_build::extended::BuildOptsExtended {
         workdir: "../community",
@@ -15,14 +12,14 @@ fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
         build_opts: Default::default(),
         build_script_opts: BuildScriptOpts {
             result_env_key: Some("BUILD_RS_SUB_BUILD_DEVHUB-COMMUNITY"),
-            rerun_if_changed_list: vec!["../discussions", "../community", "Cargo.toml", "Cargo.lock"],
+            rerun_if_changed_list: vec!["../discussions", "../community", "Cargo.toml", "../Cargo.lock"],
             build_skipped_when_env_is: vec![
                 // shorter build for `cargo check`
                 ("PROFILE", "debug"),
                 (cargo_near_build::env_keys::BUILD_RS_ABI_STEP_HINT, "true"),
             ],
-            distinct_target_dir: Some("../community-factory/target/build-rs-community-for-community-factory"),
-            stub_path: Some("../community-factory/target/stub.bin"),
+            distinct_target_dir: Some("../target/build-rs-community-for-community-factory"),
+            stub_path: Some("../target/community-stub.bin"),
         },
     };
 
--- community/build.rs
+++ community/build.rs
@@ -13,14 +13,14 @@ fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
         build_opts: Default::default(),
         build_script_opts: BuildScriptOpts {
             result_env_key: Some("BUILD_RS_SUB_BUILD_DEVHUB-DISCUSSIONS"),
-            rerun_if_changed_list: vec!["../discussions", "Cargo.toml", "Cargo.lock"],
+            rerun_if_changed_list: vec!["../discussions", "Cargo.toml", "../Cargo.lock"],
             build_skipped_when_env_is: vec![
                 // shorter build for `cargo check`
                 ("PROFILE", "debug"),
                 (cargo_near_build::env_keys::BUILD_RS_ABI_STEP_HINT, "true"),
             ],
-            distinct_target_dir: Some("../community/target/build-rs-discussions-for-community"),
-            stub_path: Some("../community/target/stub.bin"),
+            distinct_target_dir: Some("../target/build-rs-discussions-for-community"),
+            stub_path: Some("../target/discussions-stub.bin"),
         },
     };
 
