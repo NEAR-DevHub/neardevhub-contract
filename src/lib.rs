19fa5bc1a48f73fd92830d992d608a1dc9acbfe0
Thomas <thomasguntenaar@gmail.com>
2025-02-26 14:05:39 +0100

    feat: added changelog

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 pub mod access_control;
+pub mod changelog;
+pub mod common;
 pub mod community;
 pub mod debug;
 pub mod migrations;
 mod notify;
-pub mod common;
 pub mod proposal;
 pub mod rfp;
 pub mod stats;
@@ -13,10 +14,11 @@ pub mod web4;
 use crate::access_control::members::ActionType;
 use crate::access_control::members::Member;
 use crate::access_control::AccessControl;
+use changelog::*;
 use community::*;
 
 use common::*;
-use proposal::timeline::{TimelineStatusV1, TimelineStatus, VersionedTimelineStatus};
+use proposal::timeline::{TimelineStatus, TimelineStatusV1, VersionedTimelineStatus};
 use proposal::*;
 use rfp::{
     RFPId, RFPSnapshot, TimelineStatus as RFPTimelineStatus, VersionedRFP, VersionedRFPBody, RFP,
@@ -26,13 +28,13 @@ use devhub_common::{social_db_contract, SetReturnType};
 
 use near_sdk::borsh::BorshDeserialize;
 use near_sdk::collections::{LookupMap, UnorderedMap, Vector};
-use near_sdk::store::Lazy;
 use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::serde_json::{json, Number, Value};
+use near_sdk::store::Lazy;
 use near_sdk::{env, near, require, AccountId, NearSchema, PanicOnDefault, Promise};
 use web4::types::{Web4Request, Web4Response};
 
-use std::collections::{HashSet, HashMap};
+use std::collections::{HashMap, HashSet};
 use std::convert::TryInto;
 
 type PostId = u64;
@@ -59,13 +61,14 @@ pub struct Contract {
     pub communities: UnorderedMap<CommunityHandle, Community>,
     pub featured_communities: Vec<FeaturedCommunity>,
     pub available_addons: UnorderedMap<AddOnId, AddOn>,
+    pub change_log: Vector<ChangeLog>,
 }
 
 #[near]
 impl Contract {
     #[init]
     pub fn new() -> Self {
-        migrations::state_version_write(&migrations::StateVersion::V11);
+        migrations::state_version_write(&migrations::StateVersion::V12);
 
         let mut contract = Self {
             posts: Vector::new(StorageKey::Posts),
@@ -84,18 +87,16 @@ impl Contract {
             communities: UnorderedMap::new(StorageKey::Communities),
             featured_communities: Vec::new(),
             available_addons: UnorderedMap::new(StorageKey::AddOns),
+            change_log: Vector::new(StorageKey::ChangeLog),
         };
 
         contract.post_to_children.insert(&ROOT_POST_ID, &Vec::new());
         contract
     }
 
-    pub fn get_proposals(&self, ids: Option< Vec<ProposalId> >) -> Vec<VersionedProposal> {
+    pub fn get_proposals(&self, ids: Option<Vec<ProposalId>>) -> Vec<VersionedProposal> {
         if let Some(ids) = ids {
-            ids
-                .into_iter()
-                .filter_map(|id| self.proposals.get(id.into()))
-                .collect()
+            ids.into_iter().filter_map(|id| self.proposals.get(id.into())).collect()
         } else {
             self.proposals.to_vec()
         }
@@ -123,6 +124,14 @@ impl Contract {
         (0..self.rfps.len().try_into().unwrap()).collect()
     }
 
+    pub fn get_change_log(&self) -> Vec<ChangeLog> {
+        self.change_log.to_vec()
+    }
+
+    pub fn get_change_log_since(&self, since: u64) -> Vec<ChangeLog> {
+        self.change_log.iter().filter(|log| log.block_id > since).collect()
+    }
+
     #[payable]
     pub fn add_proposal(
         &mut self,
@@ -140,7 +149,10 @@ impl Contract {
                 "Terms and conditions version cannot be from the future"
             );
         } else {
-            require!(env::current_account_id() != "devhub.near", "Accepted terms and conditions version is required");
+            require!(
+                env::current_account_id() != "devhub.near",
+                "Accepted terms and conditions version is required"
+            );
         }
 
         let proposal_body = body.clone().latest_version();
@@ -261,6 +273,11 @@ impl Contract {
     ) -> BlockHeightCallbackRetValue {
         proposal.social_db_post_block_height = set_result.block_height.into();
         self.proposals.push(&proposal.clone().into());
+        self.add_change_log(ChangeLog {
+            block_id: set_result.block_height.into(),
+            changed_object_id: proposal.id,
+            change_log_type: ChangeLogType::Proposal(proposal.id),
+        });
         BlockHeightCallbackRetValue { proposal_id: proposal.id }
     }
 
@@ -269,9 +286,14 @@ impl Contract {
         #[allow(unused_mut)] mut rfp: RFP,
         #[callback_unwrap] set_result: SetReturnType,
     ) -> BlockHeightCallbackRetValue {
-        let ret_value = BlockHeightCallbackRetValue { proposal_id: rfp.id };
+        let ret_value = BlockHeightCallbackRetValue { proposal_id: rfp.id.clone() };
         rfp.social_db_post_block_height = set_result.block_height.into();
-        self.rfps.push(&rfp.into());
+        self.rfps.push(&rfp.clone().into());
+        self.add_change_log(ChangeLog {
+            block_id: set_result.block_height.into(),
+            changed_object_id: rfp.id.clone(),
+            change_log_type: ChangeLogType::RFP(rfp.id),
+        });
         ret_value
     }
 
@@ -301,7 +323,7 @@ impl Contract {
         res.sort();
         res
     }
-    
+
     pub fn get_all_authors(&self) -> Vec<AccountId> {
         let mut res: Vec<_> = self.authors.keys().collect();
         res.sort();
@@ -453,7 +475,11 @@ impl Contract {
     }
 
     #[payable]
-    pub fn edit_proposal_timeline(&mut self, id: ProposalId, timeline: TimelineStatusV1) -> ProposalId {
+    pub fn edit_proposal_timeline(
+        &mut self,
+        id: ProposalId,
+        timeline: TimelineStatusV1,
+    ) -> ProposalId {
         let proposal: Proposal = self
             .proposals
             .get(id.into())
@@ -484,7 +510,11 @@ impl Contract {
     }
 
     #[payable]
-    pub fn edit_proposal_linked_rfp(&mut self, id: ProposalId, rfp_id: Option<RFPId>) -> ProposalId {
+    pub fn edit_proposal_linked_rfp(
+        &mut self,
+        id: ProposalId,
+        rfp_id: Option<RFPId>,
+    ) -> ProposalId {
         let proposal: Proposal = self
             .proposals
             .get(id.into())
@@ -507,12 +537,20 @@ impl Contract {
     }
 
     #[payable]
-    pub fn cancel_rfp(&mut self, id: RFPId, proposals_to_cancel: Vec<ProposalId>, proposals_to_unlink: Vec<ProposalId>) -> RFPId {
+    pub fn cancel_rfp(
+        &mut self,
+        id: RFPId,
+        proposals_to_cancel: Vec<ProposalId>,
+        proposals_to_unlink: Vec<ProposalId>,
+    ) -> RFPId {
         for proposal_id in proposals_to_cancel {
             let proposal: Proposal = self.get_proposal(proposal_id).into();
             let proposal_timeline = proposal.snapshot.body.latest_version().timeline;
             let review_status = proposal_timeline.latest_version().get_review_status().clone();
-            self.edit_proposal_versioned_timeline(proposal_id, TimelineStatus::Cancelled(review_status).into());
+            self.edit_proposal_versioned_timeline(
+                proposal_id,
+                TimelineStatus::Cancelled(review_status).into(),
+            );
         }
 
         for proposal_id in proposals_to_unlink {
@@ -536,7 +574,8 @@ impl Contract {
     }
 
     pub fn get_global_labels(&self) -> Vec<LabelInfoExtended> {
-        let mut result: Vec<LabelInfoExtended> = self.global_labels_info
+        let mut result: Vec<LabelInfoExtended> = self
+            .global_labels_info
             .iter()
             .map(|(label, label_info)| LabelInfoExtended {
                 value: label.clone(),
@@ -549,9 +588,7 @@ impl Contract {
     }
 
     pub fn get_rfp_linked_proposals(&self, rfp_id: RFPId) -> Vec<ProposalId> {
-        self.get_linked_proposals_in_rfp(rfp_id)
-            .into_iter()
-            .collect()
+        self.get_linked_proposals_in_rfp(rfp_id).into_iter().collect()
     }
 
     #[payable]
@@ -884,10 +921,13 @@ impl Contract {
     pub fn web4_get(&self, request: Web4Request) -> Web4Response {
         web4::handler::web4_get(self, request)
     }
-    
+
     pub fn set_social_db_profile_description(&self, description: String) -> Promise {
         let editor = env::predecessor_account_id();
-        require!(editor == env::current_account_id() || self.has_moderator(editor), "Permission denied");
+        require!(
+            editor == env::current_account_id() || self.has_moderator(editor),
+            "Permission denied"
+        );
         social_db_contract()
             .with_static_gas(env::prepaid_gas().saturating_div(3))
             .with_attached_deposit(env::attached_deposit())
